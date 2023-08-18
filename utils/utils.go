3404e37378e1deb93d61f4a0f2c1fd41ca30e346
ahmetlutfu <ah.metlutfu@gmail.com>
2023-08-18 13:50:19 +0300

    LessThanExist and FindFirstBreakPoint

--- utils/utils.go
+++ utils/utils.go
@@ -8,3 +8,21 @@ func Contains[T comparable](elems []T, v T) bool {
 	}
 	return false
 }
+
+func LessThanExist[T int | uint](elems []T, from, to T) bool {
+	for _, s := range elems {
+		if s > from && s < to {
+			return true
+		}
+	}
+	return false
+}
+
+func FindFirstBreakPoint[T int | uint](elems []T) (index int) {
+	for i, elem := range elems {
+		if !LessThanExist(elems, elem, elem*2) {
+			return i
+		}
+	}
+	return -1
+}
